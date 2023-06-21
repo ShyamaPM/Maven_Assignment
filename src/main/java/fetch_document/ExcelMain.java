5dfeb9071c6bfc19b62111a828d9d586fc7572c8
ShyamaPM <shyamapm94@gmail.com>
2023-06-21 21:12:45 +0530

    First commit

---
+++ src/main/java/fetch_document/ExcelMain.java
@@ -0,0 +1,16 @@
+package fetch_document;
+
+import java.io.IOException;
+
+public class ExcelMain {
+
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("User Name: "+ExcelCode.readUserName(1,0));
+		System.out.println("Password: "+ExcelCode.readPassword(1, 1));
+
+
+	}
+
+}
